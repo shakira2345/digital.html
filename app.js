eb33bbd0c397a5af750c8f2e01ea15871b21cbb7
shakira2345 <154341626+shakira2345@users.noreply.github.com>
2024-03-18 10:13:33 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+a=10;
+
+b=10;
+if(a<b){
+    console.log("a less than b");
+
+}
+else if(a==b){
+    console.log("a equal to b")
+}
+else{
+    console.log("a is not equals");
+}
+let name="maheer";
+let lastname=" siddiui";
+console.log(name+lastname);
+
+
